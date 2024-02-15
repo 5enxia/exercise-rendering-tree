ba037eb4ec127c59c6e3e550c06ba84e5948b110
5enxia <takayasumorishita@gmail.com>
2024-02-15 20:44:16 +0900

    Add mock of to_layout_box()

--- src/layout.rs
+++ src/layout.rs
@@ -1,3 +1,33 @@
+
+use crate::style::{Display, PropertyMap, StyledNode};
+use crate::dom::NodeType;
+
+#[derive(Debug, PartialEq)]
+pub struct LayoutBox<'a> {
+    pub box_type: BoxType<'a>,
+    pub children: Vec<LayoutBox<'a>>,
+}
+
+#[derive(Debug, PartialEq)]
+pub enum BoxType<'a> {
+    BlockBox(BoxProps<'a>),
+    InlineBox(BoxProps<'a>),
+    AnonymousBox,
+}
+
+#[derive(Debug, PartialEq)]
+pub struct BoxProps<'a> {
+    pub node_type: &'a NodeType,
+    pub properties: PropertyMap,
+}
+
+pub fn to_layout_box<'a>(snode: StyledNode<'a>) -> LayoutBox<'a> {
+    LayoutBox {
+        box_type: BoxType::AnonymousBox,
+        children: vec![]
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use crate::{css::CSSValue, dom::Element};
