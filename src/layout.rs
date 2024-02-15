e4140d4be005211b0cb6b01d8a24ec2c8e82a7f0
5enxia <takayasumorishita@gmail.com>
2024-02-15 20:32:19 +0900

    copy layout tests from answer

---
+++ src/layout.rs
@@ -0,0 +1,120 @@
+#[cfg(test)]
+mod tests {
+    use crate::{css::CSSValue, dom::Element};
+
+    use super::*;
+
+    #[test]
+    fn test_to_layout_box() {
+        let block = [(
+            "display".to_string(),
+            CSSValue::Keyword("block".to_string()),
+        )];
+        let inline = [(
+            "display".to_string(),
+            CSSValue::Keyword("inline".to_string()),
+        )];
+
+        let node = NodeType::Element(Element {
+            tag_name: "div".into(),
+            attributes: [].iter().cloned().collect(),
+        });
+        let snode = StyledNode {
+            node_type: &node,
+            properties: block.iter().cloned().collect(),
+            children: vec![
+                StyledNode {
+                    node_type: &node,
+                    properties: block.iter().cloned().collect(),
+                    children: vec![],
+                },
+                StyledNode {
+                    node_type: &node,
+                    properties: inline.iter().cloned().collect(),
+                    children: vec![
+                        StyledNode {
+                            node_type: &node,
+                            properties: block.iter().cloned().collect(),
+                            children: vec![],
+                        },
+                        StyledNode {
+                            node_type: &node,
+                            properties: block.iter().cloned().collect(),
+                            children: vec![],
+                        },
+                    ],
+                },
+                StyledNode {
+                    node_type: &node,
+                    properties: inline.iter().cloned().collect(),
+                    children: vec![],
+                },
+                StyledNode {
+                    node_type: &node,
+                    properties: block.iter().cloned().collect(),
+                    children: vec![],
+                },
+            ],
+        };
+
+        assert_eq!(
+            to_layout_box(snode),
+            LayoutBox {
+                box_type: BoxType::BlockBox(BoxProps {
+                    node_type: &node,
+                    properties: block.iter().cloned().collect(),
+                }),
+                children: vec![
+                    LayoutBox {
+                        box_type: BoxType::BlockBox(BoxProps {
+                            node_type: &node,
+                            properties: block.iter().cloned().collect(),
+                        }),
+                        children: vec![],
+                    },
+                    LayoutBox {
+                        box_type: BoxType::AnonymousBox,
+                        children: vec![
+                            LayoutBox {
+                                box_type: BoxType::InlineBox(BoxProps {
+                                    node_type: &node,
+                                    properties: inline.iter().cloned().collect(),
+                                }),
+                                children: vec![
+                                    LayoutBox {
+                                        box_type: BoxType::BlockBox(BoxProps {
+                                            node_type: &node,
+                                            properties: block.iter().cloned().collect(),
+                                        }),
+                                        children: vec![],
+                                    },
+                                    LayoutBox {
+                                        box_type: BoxType::BlockBox(BoxProps {
+                                            node_type: &node,
+                                            properties: block.iter().cloned().collect(),
+                                        }),
+                                        children: vec![],
+                                    }
+                                ],
+                            },
+                            LayoutBox {
+                                box_type: BoxType::InlineBox(BoxProps {
+                                    node_type: &node,
+                                    properties: inline.iter().cloned().collect(),
+                                }),
+                                children: vec![],
+                            }
+                        ]
+                    },
+                    LayoutBox {
+                        box_type: BoxType::BlockBox(BoxProps {
+                            node_type: &node,
+                            properties: block.iter().cloned().collect(),
+                        }),
+                        children: vec![],
+                    }
+                ],
+            }
+        );
+    }
+}
