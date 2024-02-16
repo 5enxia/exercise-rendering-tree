3f97802949b025f32e26cbf6a7b6a30ea00d09aa
5enxia <takayasumorishita@gmail.com>
2024-02-16 19:35:19 +0900

    Copy unit test from answer

---
+++ src/javascript.rs
@@ -0,0 +1,38 @@
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_execute() {
+        let mut runtime = JavaScriptRuntime::new();
+        {
+            // a simple math
+            let r = runtime.execute("", "1 + 1");
+            assert!(r.is_ok());
+            assert_eq!(r.unwrap(), "2");
+        }
+        {
+            // simple string operation
+            let r = runtime.execute("", "'test' + \"func\" + `012${1+1+1}`");
+            assert!(r.is_ok());
+            assert_eq!(r.unwrap(), "testfunc0123");
+        }
+        {
+            // use of undefined variable
+            let r = runtime.execute("", "test");
+            assert!(r.is_err());
+        }
+        {
+            // lambda definition
+            let r = runtime.execute("", "let inc = (i) => { return i + 1 }; inc(1)");
+            assert!(r.is_ok());
+            assert_eq!(r.unwrap(), "2");
+        }
+        {
+            // variable reuse
+            let r = runtime.execute("", "inc(4)");
+            assert!(r.is_ok());
+            assert_eq!(r.unwrap(), "5");
+        }
+    }
+}
